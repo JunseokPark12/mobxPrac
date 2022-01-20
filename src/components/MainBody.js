da6892d95ff63545f84c1faee87d0db7ad618a15
JunseokPark12 <pjs@huray.net>
2022-01-20 10:17:48 +0900

    Feat : 사용하지 않을 Mobx 관련 코드 삭제

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -1,19 +1,9 @@
-import { observer } from 'mobx-react';
-import useStores from '../hooks/useStores';
-
 function MainBody() {
-  const { countStore } = useStores();
   return (
     <div>
-      <p>MainBody {countStore.count}</p>
-      <p>
-        <button onClick={countStore.increase}>+</button>
-      </p>
-      <p>
-        <button onClick={countStore.decrease}>-</button>
-      </p>
+      <p>MainBody </p>
     </div>
   );
 }
 
-export default observer(MainBody);
+export default MainBody;
